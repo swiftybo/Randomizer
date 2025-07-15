db0a9832e4a0d15686d4b9bd50253f7039884393
swiftybo <leungjordan28@gmail.com>
2025-07-15 16:07:42 +0100

    added in html and js code to render UI for film list and also used movieposter API to generate posters of movies in the movielist

---
+++ films.js
@@ -0,0 +1,62 @@
+const filmList = [
+    { title: "The Godfather", year: 1972, status: "not watched" },
+    { title: "12 Angry Men", year: 1957, status: "not watched" },
+    { title: "Attack on Titan", year: 2015, status: "not watched" },
+    { title: "10 Cloverfield Lane", year: 2016, status: "not watched" },
+    { title: "Memento", year: 2000, status: "not watched" },
+    { title: "Shutter Island", year: 2010, status: "not watched" },
+    {
+        title: "Spider-Man: Into the Spider-Verse",
+        year: 2018,
+        status: "not watched",
+    },
+    {
+        title: "Sweeney Todd: The Demon Barber of Fleet Street",
+        year: 2007,
+        status: "not watched",
+    },
+    { title: "A Star is Born", year: 2018, status: "not watched" },
+    { title: "Your Name.", year: 2016, status: "not watched" },
+];
+
+function displayFilms(films) {
+    filmTitles = films.map((film) => film.title);
+
+    console.log(filmTitles);
+}
+
+// displayFilms(filmList);
+
+export function renderUI() {
+    console.log("Rendering display");
+    const html = `<button class="btn__display">Display</button>
+            <button class="btn__random">Randomize!!</button>`;
+
+    content.insertAdjacentHTML("afterend", html);
+}
+
+displayBtn.addEventListener("click", function () {
+    displayFilms(filmList);
+});
+
+function getPosters(filmList) {
+    filmList.forEach((film) => {
+        const Poster = document.createElement("img");
+        Poster.classList.add("moviePoster");
+
+        const filmStr = encodeURIComponent(film.title)
+            .toLowerCase()
+            .replace(" ", "+");
+        const filmReleaseDate = film.year;
+
+        fetch(
+            `http://www.omdbapi.com/?t=${filmStr}&y=${filmReleaseDate}&apikey=6f14816c`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                Poster.src = data.Poster;
+
+                content.appendChild(Poster);
+            });
+    });
+}
--- script.js
+++ script.js
@@ -2,31 +2,42 @@
 
 // Practice with tabs
 
+import { renderUI } from "./films.js";
+
 const tabContents = document.querySelectorAll(".tabcontent");
 const tabLinks = document.querySelectorAll(".tablink");
+const content = document.getElementById("content");
+const displayBtn = document.querySelector(".btn__display");
+const filmButton = document.getElementById("filmtab");
 
-const openColour = function (colour) {
-    // Hide all tab content
-    tabContents.forEach((tab) => {
-        return (tab.style.display = "none");
-    });
+// // Practice with tabs
+// const openColour = function (colour) {
+//     // Hide all tab content
+//     tabContents.forEach((tab) => {
+//         return (tab.style.display = "none");
+//     });
 
-    // Remove active tab styling
-    tabLinks.forEach((tab) => {
-        return tab.classList.remove("active");
-    });
+//     // Remove active tab styling
+//     tabLinks.forEach((tab) => {
+//         return tab.classList.remove("active");
+//     });
 
-    // Display tab content of colour
-    const colourContent = document.getElementById(colour);
-    colourContent.style.display = "block";
-};
+//     // Display tab content of colour
+//     const colourContent = document.getElementById(colour);
+//     colourContent.style.display = "block";
+// };
 
-tabLinks.forEach((tab) => {
-    tab.addEventListener("click", function (event) {
-        const colour = tab.textContent.toLowerCase();
-        openColour(colour);
+// tabLinks.forEach((tab) => {
+//     tab.addEventListener("click", function (event) {
+//         const colour = tab.textContent.toLowerCase();
+//         openColour(colour);
 
-        // console.log(event.currentTarget);
-        event.currentTarget.classList.add("active");
-    });
+//         // console.log(event.currentTarget);
+//         event.currentTarget.classList.add("active");
+//     });
+// });
+
+filmButton.addEventListener("click", function () {
+    console.log("film tab clicked");
+    renderUI();
 });
